9143995c03b6bc54510b033a1c4b4c387dbd3998
Mohammed Mujeebuddin <138692333+Mohammed2327@users.noreply.github.com>
2024-10-24 11:53:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
@@ -6,23 +7,15 @@ import Cart from './components/Cart';
 import Checkout from './components/Checkout';
 import { CartProvider } from './components/CartContext';
 
-const Layout = () => {
+const App = () => {
     return (
-    <>
+        <CartProvider>
             <Navbar />
             <Routes>
                 <Route path="/" element={<ProductList />} />
                 <Route path="/cart" element={<Cart />} />
                 <Route path="/checkout" element={<Checkout />} />
             </Routes>
-        </>
-    );
-};
-
-const App =() =>{
-    return(
-        <CartProvider>
-            <Layout />
         </CartProvider>
     );
 };
